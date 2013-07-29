486cb94e40654614ef3c13a4a157226161823611
Attilio <atti84it@yahoo.it>
2013-07-29 11:30:05 -0500

    First commit + Recipe model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tahini::Application.config.secret_key_base = '89872e148d915792f655f8f11bba1f4512b2393d6050d690031b1038e0e11ed57c6d51cdba8b5d8064be7e5edfab359b0786510e563a5fdd2cad46be6831dd8f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tahini::Application.config.session_store :cookie_store, key: '_tahini_session'
