07d686cecff54caad1211fae98dd3f837cca7d41
Attilio <atti84it@yahoo.it>
2013-08-14 09:08:30 -0500

    Written new failing (!) test

--- test/models/ingredient_tu.rb
+++ test/models/ingredient_tu.rb
@@ -36,18 +36,23 @@ class TestIngredient < Test::Unit::TestCase
     assert_equal(32, i.number)
     assert_equal(nil, i.unit)
     assert_equal('sacchetti pane raffermo', i.name)
-    
+
     i = Ingredient.new_from_string("300.0 g harina de trigo", nil)
     assert_equal(300, i.number)
     assert_equal('g', i.unit)
-    assert_equal('harina de trigo', i.name)    
+    assert_equal('harina de trigo', i.name)
+
+    i = Ingredient.new_from_string("2.5 gr nuez moscada", nil)
+    assert_equal(2.5, i.number)
+    assert_equal('gr', i.unit)
+    assert_equal('nuez moscada', i.name)
   end
 
   def test_parse_amount
     i = Ingredient.new
     assert_equal(35.6, i.send(:parse_amount, 35.6))
     assert_equal(1, i.send(:parse_amount, '1'))
-    assert_equal(0.2, i.send(:parse_amount, '1/5'))    
+    assert_equal(0.2, i.send(:parse_amount, '1/5'))
     assert_equal(3.5, i.send(:parse_amount, '3 1/2'))
     assert_raise (StandardError) { i.send(:parse_amount, '2 4 1/2') }
   end
