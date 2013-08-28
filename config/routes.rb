e593f4c5387be8d05ec0b50a282ad2667a4de21c
Attilio <atti84it@yahoo.it>
2013-08-27 19:52:18 -0500

    Root to recipes#index

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Tahini::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'recipes#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
